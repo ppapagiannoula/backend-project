16fa699fd4e7dcc0a9ec9c450a020c16a518af16
Paraskevi Papagiannoula <83019597+ppapagiannoula@users.noreply.github.com>
2023-03-29 19:09:52 +0300

    products.js

--- controllers/products.js
+++ controllers/products.js
@@ -2,17 +2,19 @@
 
 
 exports.getAddProduct = (req,res,next) => {
-    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">submit</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
 
 }
 
 exports.postAddProduct =(req,res,next) => {
     //save to database
+    //sotre product to database
     res.redirect('/');
 }
 
 exports.getAllProducts = (req,res,next)=>{
     res.send(<h1>welcome to our eshop</h1>)
+    //fetch all products from database
 }
 
 // exports.getProduct = (req,res,next) => {
