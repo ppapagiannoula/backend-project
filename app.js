681ea8b1296d497fdd961ede2c00350bd19914ae
Paraskevi Papagiannoula <83019597+ppapagiannoula@users.noreply.github.com>
2023-04-10 17:31:28 +0300

    6

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-// const http = require('http');
-const express = require('express');
-// const bodyParser =require('body-parser');
-const errorController = require('./controllers/error');
 const path = require('path');
+
+const express = require('express');
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views','views');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// app.use(express.json()); 
-app.use(express.urlencoded({extended: false})); //true: precises that the req.body object will contain values of any type instead of just strings
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname,'public'))); //Needs to be static for using vanilla css/js on templating engines. We use path npm package to create paths easy
-
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404Error);
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,33 @@
+const path = require('path');
+
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
+// / => GET
+router.get('/', shopController.getIndex);
 
-// / => GET root page
-router.get('/',shopController.getIndex);
-router.get('/product-list', shopController.getAllProducts);
-router.get('/cart', shopController.getCart);
+// /products => GET
+router.get('/products', shopController.getAllProducts);
+
+// /products/productId => GET
+router.get('/products/:productId', shopController.getProduct);
+
+// /cart => GET
+ router.get('/cart', shopController.getCart);
+
+// /cart => POST
+/* router.post('/cart', shopController.postCart); */
+
+// /cart-delete-item => POST
+/* router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+ */
+// /orders => GET
 router.get('/orders', shopController.getOrders);
-router.get('/checkout', shopController.getCheckout);
 
-router.get('/products/:productId', shopController.getProduct); //get the specific product
+// /checkout => GET
+router.get('/checkout', shopController.getCheckout); 
+
 module.exports = router;
